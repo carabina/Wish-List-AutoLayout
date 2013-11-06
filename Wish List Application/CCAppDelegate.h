dd3d8528c331e4d8c6d40a773e9cac9207bb346b
Eliot Arntz <arntzel@gmail.com>
2013-11-06 14:25:44 +0100

    Initial Commit

---
+++ Wish List Application/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Wish List Application
+//
+//  Created by Eliot Arntz on 11/6/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wish List Application/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Wish List Application
+//
+//  Created by Eliot Arntz on 11/6/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
